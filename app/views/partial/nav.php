2ded14c38ceaa46db927959be4ea2d0b894f1343
ianfleon <ianfleon08@gmail.com>
2021-06-03 14:40:09 +0900

    update

--- app/views/partial/nav.php
+++ app/views/partial/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-navku fixed-top">
     <div class="container">
         <!-- Brand nav -->
-        <a class="navbar-brand label-section" href="#">PARIBURSEL</a>
+        <a class="navbar-brand label-section" href="index.php">PARIBURSEL</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="galeri.php">Galeri</a>
@@ -20,6 +20,9 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="profil.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-outline-putih" href="profil.php">Hubungi Kami</a>
                     </li>
                 </ul>
             </div>
