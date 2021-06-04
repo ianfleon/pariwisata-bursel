f8f52b32fa19f7b368d288e654f689d0f8074820
ianfleon <ianfleon08@gmail.com>
2021-06-04 15:42:36 +0900

    memperbaiki tampilan

--- admin/artikel/form-post.php
+++ admin/artikel/form-post.php
@@ -1,42 +1,45 @@
-<div class="col-md-6">
-            <!-- general form elements -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Quick Example</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                  </div>
+<div class="col-md-12">
+    <!-- general form elements -->
+    <div class="card card-dark">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Artikel Baru</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="status">Status</label>
+                    <select name="status" id="status" class="form-control">
+                        <option value="1">Publish</option>
+                        <option value="0">Draft</option>
+                    </select>
                 </div>
-                <!-- /.card-body -->
+                <div class="form-group">
+                    <label for="judul">Judul Artikel</label>
+                    <input type="text" class="form-control" id="judul" placeholder="Masukan Judul">
+                </div>
+                <div class="form-group">
+                    <label for="judul">Isi Artikel</label>
+                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                    <span class="text-muted">*Tulis isi artikel anda disini.</span>
+                </div>
 
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group">
+                    <label for="exampleInputFile">Cover Artikel</label>
+                    <div class="input-group">
+                        <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Pilih gambar</label>
+                    </div>
                 </div>
-              </form>
+                    <p class="text-muted">*Gambar sampul untuk artikel</p>
+                </div>
             </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-plane"></i> Post!</button>
+            </div>
+        </form>
+    </div>
+</div>
