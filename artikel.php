cb6f2541a1ef4576c824a7d888aaf40b8b637aed
ianfleon <ianfleon08@gmail.com>
2021-06-10 16:20:18 +0900

    perbaiki sa

--- artikel.php
+++ artikel.php
@@ -53,6 +53,22 @@
 	<?php endfor; ?>
 </div>
 
+<!-- <nav aria-label="...">
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active" aria-current="page">
+      <a class="page-link" href="#">2</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav> -->
+
 <?php endif; ?>
 <!-- //endif -->
 </div>
