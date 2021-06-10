fa2709e06e4f1800668c01ea891e7cbff6386fe6
ianfleon <ianfleon08@gmail.com>
2021-06-10 17:14:12 +0900

    ubah tampilan galeri

--- galeri.php
+++ galeri.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <!-- ++ Header -->
+<head>
 <?php require_once 'app/views/partial/head.php' ?>
-
+</head>
 <body>
 <!-- ++ Navbar -->
 <?php require_once 'app/views/partial/nav.php' ?>
+
+<div class="container mt-7 mb-5">
+
+<div class="masonry">
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1606787619248-f301830a5a57?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1623100603721-ce6d0c64bf56?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1623300025008-ccbe3e8501aa?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1623270753512-46542a493d66?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1623139769888-7b2473ea1629?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+	<div class="item">
+		<img src="https://images.unsplash.com/photo-1623307019152-1ee797183f1d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+	</div>
+</div>
+
+</div>
 
 <!-- ++ Footer -->
 <?php require_once 'app/views/partial/foot.php' ?>
