eb383b4492ea96a777688497c5580b2d03424cac
ianfleon <ianfleon08@gmail.com>
2021-06-16 11:53:54 +0900

    perbaiki tampilan nav

--- app/views/admin/header.php
+++ app/views/admin/header.php
@@ -109,7 +109,7 @@
             </a>
           </li>
 
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -457,7 +457,7 @@
                   <i class="far fa-circle nav-icon"></i>
                   <p>Starter Page</p>
                 </a>
-              </li>
+              </li> -->
             </ul>
           </li>
         </ul>
