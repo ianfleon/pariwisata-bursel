cf3d8df7a3c22d8baec195f2dd30f09369751913
ianfleon <ianfleon08@gmail.com>
2021-06-22 15:28:07 +0900

    perbaiki sidebar admin

--- app/views/admin/footer.php
+++ app/views/admin/footer.php
@@ -17,11 +17,6 @@
 <!-- jQuery -->
 <script src="<?= BASE_URL ?>/admin/plugins/jquery/jquery.min.js"></script>
 
-<?= BASE_URL ?>
-
-<!-- jQuery UI 1.11.4 -->
-<script src="<?= BASE_URL ?>/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
-
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -30,39 +25,40 @@
 <!-- Bootstrap 4 -->
 <script src="<?= BASE_URL ?>/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+<!-- AdminLTE App -->
+<script src="<?= BASE_URL ?>/admin/dist/js/adminlte.js"></script>
+
+<!-- jQuery UI 1.11.4 -->
+<!-- <script src="<?= BASE_URL ?>/admin/plugins/jquery-ui/jquery-ui.min.js"></script> -->
+
 <!-- Sparkline -->
-<scrsipt src="<?= BASE_URL ?>/admin/plugins/sparklines/sparkline.js"></scrsipt>
+<!-- <script src="<?= BASE_URL ?>/admin/plugins/sparklines/sparkline.js"></script> -->
 
 <!-- JQVMap -->
-<!-- <scrsipt src="plugins/jqvmap/jquery.vmap.min.js"></scrsipt> -->
+<!-- <script src="plugins/jqvmap/jquery.vmap.min.js"></script> -->
 
-<!-- <scrsipt src="plugins/jqvmap/maps/jquery.vmap.usa.js"></scrsipt> -->
+<!-- <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script> -->
 
 <!-- jQuery Knob Chart -->
-<!-- <scrsipt src="plugins/jquery-knob/jquery.knob.min.js"></scrsipt> -->
+<!-- <script src="plugins/jquery-knob/jquery.knob.min.js"></script> -->
 
 <!-- daterangepicker -->
-<!-- <scrsipt src="plugins/moment/moment.min.js"></scrsipt> -->
+<!-- <script src="plugins/moment/moment.min.js"></script> -->
 
-<!-- <scrsipt src="plugins/daterangepicker/daterangepicker.js"></scrsipt> -->
+<!-- <script src="plugins/daterangepicker/daterangepicker.js"></script> -->
 
 <!-- Tempusdominus Bootstrap 4 -->
-<scrsipt src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></scrsipt>
-
-<!-- Summernote -->
-<scrsipt src="plugins/summernote/summernote-bs4.min.js"></scrsipt>
+<!-- <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script> -->
 
 <!-- overlayScrollbars -->
-<scrsipt src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></scrsipt>
+<!-- <script src="<?= BASE_URL ?>/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
 
-<!-- AdminLTE App -->
-<scrsipt src="dist/js/adminlte.js"></scrsipt>
 
 <!-- AdminLTE for demo purposes -->
-<scrsipt src="dist/js/demo.js"></scrsipt>
+<!-- <script src="dist/js/demo.js"></script> -->
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<scrsipt src="dist/js/pages/dashboard.js"></scrsipt>
+<!-- <script src="<?= BASE_URL ?>/admin/dist/js/pages/dashboard.js"></script> -->
 
 </body>
 </html>
