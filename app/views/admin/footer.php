b8f71ae9ddfc335f4021dc9e71793c5588eb2eae
ianfleon <ianfleon08@gmail.com>
2021-06-22 15:17:03 +0900

    perbaiki title page di admin

--- app/views/admin/footer.php
+++ app/views/admin/footer.php
@@ -1,51 +1,68 @@
-  <footer class="main-footer">
+<footer class="main-footer">
     <strong>Pariwisata Buru Selatan, 2021.</strong>
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Provinsi Maluku</b>
-    </div>
-  </footer>
+        <div class="float-right d-none d-sm-inline-block">
+          <b>Provinsi Maluku</b>
+        </div>
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+<!-- Control sidebar content goes here -->
+</aside>
+
+<!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="<?= BASE_URL ?>/admin/plugins/jquery/jquery.min.js"></script>
+
+<?= BASE_URL ?>
+
 <!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?= BASE_URL ?>/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
+
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
+
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-ChartJS
-<!-- <script src="plugins/chart.js/Chart.min.js"></script> -->
+<script src="<?= BASE_URL ?>/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 <!-- Sparkline -->
-<scrsipt src="plugins/sparklines/sparkline.js"></scrsipt>
+<scrsipt src="<?= BASE_URL ?>/admin/plugins/sparklines/sparkline.js"></scrsipt>
+
 <!-- JQVMap -->
-<scrsipt src="plugins/jqvmap/jquery.vmap.min.js"></scrsipt>
-<scrsipt src="plugins/jqvmap/maps/jquery.vmap.usa.js"></scrsipt>
+<!-- <scrsipt src="plugins/jqvmap/jquery.vmap.min.js"></scrsipt> -->
+
+<!-- <scrsipt src="plugins/jqvmap/maps/jquery.vmap.usa.js"></scrsipt> -->
+
 <!-- jQuery Knob Chart -->
-<scrsipt src="plugins/jquery-knob/jquery.knob.min.js"></scrsipt>
+<!-- <scrsipt src="plugins/jquery-knob/jquery.knob.min.js"></scrsipt> -->
+
 <!-- daterangepicker -->
-<scrsipt src="plugins/moment/moment.min.js"></scrsipt>
-<scrsipt src="plugins/daterangepicker/daterangepicker.js"></scrsipt>
+<!-- <scrsipt src="plugins/moment/moment.min.js"></scrsipt> -->
+
+<!-- <scrsipt src="plugins/daterangepicker/daterangepicker.js"></scrsipt> -->
+
 <!-- Tempusdominus Bootstrap 4 -->
 <scrsipt src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></scrsipt>
+
 <!-- Summernote -->
 <scrsipt src="plugins/summernote/summernote-bs4.min.js"></scrsipt>
+
 <!-- overlayScrollbars -->
 <scrsipt src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></scrsipt>
+
 <!-- AdminLTE App -->
 <scrsipt src="dist/js/adminlte.js"></scrsipt>
+
 <!-- AdminLTE for demo purposes -->
 <scrsipt src="dist/js/demo.js"></scrsipt>
+
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <scrsipt src="dist/js/pages/dashboard.js"></scrsipt>
+
 </body>
 </html>
--- app/views/admin/header.php
+++ app/views/admin/header.php
@@ -7,14 +7,30 @@ if (!isset($_SESSION['admin_login'])) {
     header('Location: login.php');
 }
 
-require_once '../app/config/handler.php'; // main function ?>
+require_once '../app/config/handler.php'; // main function
+
+// Base URL
+define("BASE_URL", "http://localhost/pariwisata-bursel");
+
+// Title Halaman berdasarkan File
+$title = explode('/', $_SERVER["SCRIPT_NAME"]);
+$title = end($title);
+$title = explode('.php', $title)[0];
+
+if ($title == 'index') {
+    $title = "Dashboard";
+}
+
+$title[0] = strtoupper($title[0]);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title><?= $title ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
